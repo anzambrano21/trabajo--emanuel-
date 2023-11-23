ad7d863b893059c10693a554c9d39be96f2894fd
anzambrano21 <lolepico1234@gmail.com>
2023-11-23 19:39:49 -0400

    first commit

---
+++ contadores.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5 import  uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow,QApplication
+import mysql.connector
+
+class conecxion:
+    def __init__(self):
+        conexion=mysql.connector.connect(user='root',password='',host='localhost',database='seniat2023',port='3306')
+        
+
+class contador(QMainWindow):
+    def __init__(self):
+        self.ban=False
+        super().__init__()
+        uic.loadUi("lapiz.ui",self)
+        #una lista de los objeto qt de libro de compra
+        self.licom=[self.numFac,self.conFac,self.DocAfec,self.dateEdit_3,self.dateEdit_4,self.Rif,self.Cliente,self.montoIncli,self.exten,self.baseIm,self.ISVimpor,self.basenacio,self.ISVnacio,self.lineEdit_14,self.comboBox_4,self.comboBox_5,self.comboBox_6]
+    def keyPressEvent(self,event):
+        if event.key()==Qt.Key_Return:
+            for i in range(len(self.licom)):
+                if self.licom[i].hasFocus():
+                    # Si es el último QTextEdit en la lista, mueve el foco al primero
+                    if i == len(self.licom) - 1:
+                        self.licom[0].setFocus()
+                    else:
+                     
+                    # De lo contrario, mueve el foco al siguiente QTextEdit
+                        self.licom[i+1].setFocus()
+
+                    break
+
+
+
+app=QApplication(sys.argv)
+GUI=contador()
+GUI.show()
+sys.exit(app.exec_())
