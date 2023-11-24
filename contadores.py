1cd803587901abbca951dba5941c4e660b053566
anzambrano21 <lolepico1234@gmail.com>
2023-11-24 17:58:47 -0400

    yes

--- contadores.py
+++ contadores.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import  uic
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow,QApplication
+from PyQt5.QtCore import Qt,QDate
+from PyQt5.QtWidgets import QMainWindow,QApplication,QTableWidget,QTableWidgetItem
 import mysql.connector
 from PyQt5 import QtWidgets
 
@@ -15,6 +15,8 @@ class contador(QMainWindow):
         self.con=conecxion()
         super().__init__()
         uic.loadUi("lapiz.ui",self)
+        self.tableWidget_2.selectionModel().selectionChanged.connect(self.fila_seleccionada)     
+        self.cartablaliCom()
         #una lista de los objeto qt de libro de compra
         self.licom=[self.numFac,self.conFac,self.DocAfec,self.dateEdit_3,self.dateEdit_4,self.Rif,self.Cliente,self.montoIncli,self.exten,self.baseIm,self.ISVimpor,self.basenacio,self.ISVnacio,self.comboBox_4,self.comboBox_5,self.comboBox_6]
     def keyPressEvent(self,event):
@@ -39,10 +41,8 @@ class contador(QMainWindow):
                 dat=self.licom[i].date()
                 vec.append(dat.toPyDate())
                 print(vec[i])
-                
             elif isinstance(self.licom[i],QtWidgets.QComboBox):
                 vec.append(self.licom[i].currentText())
-
         return vec
     def guardar(self):
         # Crear un cursor
@@ -53,13 +53,38 @@ class contador(QMainWindow):
         # Confirmar la transacción
         self.con.conexion.commit()
     def cartablaliCom(self):
+        self.tabla=self.findChild(QTableWidget, 'tableWidget_2') 
         cursor =self.con.conexion.cursor()
-        cursor.execute("SELECT * FROM libcom")
+        cursor.execute("SELECT id,numfactur,docafectado,fechafactur,fechafactura,rif,proveedor,montoimputotal,exentas,baseimportacion,impuimportacion,basenacional,impunacional,usuario,pornacional,porimportacion,poreten,comproreten,fecharetencion,facPolar,fechafactura FROM libcom")
+        
         results = cursor.fetchall()
-        self.table.setRowCount(len(results))
+        
+        self.tabla.setRowCount(len(results))
+        self.tabla.setColumnCount(len(results[0]))
         for i, row in enumerate(results):
             for j, item in enumerate(row):
-                self.tableWidget_2.setItem(i, j, self.QTableWidgetItem(str(item)))
+                self.tabla.setItem(i, j, QTableWidgetItem(str(item)))
+    def fila_seleccionada(self):
+        rows = self.tableWidget_2.currentRow()+1
+        cursor =self.con.conexion.cursor()
+        cursor.execute("SELECT numfactur,controlFac,docafectado,DATE(fechafactur),DATE(fechafactura),Rif,cliente,montoimputotal,exentas,baseimportacion,impuimportacion,basenacional,ISVnacional,facPolar,documento,impunacional FROM libcom WHERE "+str(rows)+"=id")
+        results = cursor.fetchall()
+        
+        for i in range(len(self.licom)):
+            
+            if(isinstance(self.licom[i],QtWidgets.QLineEdit)):
+                self.licom[i].setText(str(results[0][i]))
+            elif isinstance(self.licom[i],QtWidgets.QDateEdit):
+                date = QDate(results[0][i])
+                self.licom[i].setDate(date)
+            elif isinstance(self.licom[i],QtWidgets.QComboBox):
+                 if isinstance(results[0][i], float):
+                    res=int(results[0][i])
+                    self.licom[i].setCurrentText(str(res))
+                    break
+                 
+                 self.licom[i].setCurrentText(str(results[0][i]))
+        
 
 app=QApplication(sys.argv)
 GUI=contador()
